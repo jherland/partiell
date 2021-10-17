2587dfe14e4a856f86c05f752381181d7b0941ae
Johan Herland <johan@herland.net>
2021-10-17 16:25:07 +0200

    Prepare for v0.0.2 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="partiell",
-    version="0.0.1",
+    version="0.0.2",
     description="Partial function application with '...'",
     long_description=long_description,
     long_description_content_type="text/markdown",
